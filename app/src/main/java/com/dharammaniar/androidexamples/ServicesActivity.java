09e79425e47d71973b78d92c61fb7a36f7666f70
Dharam Maniar <dharammaniar@hotmail.com>
2016-02-24 12:38:46 -0500

    Refactor Service Activity Layout

--- app/src/main/java/com/dharammaniar/androidexamples/ServicesActivity.java
+++ app/src/main/java/com/dharammaniar/androidexamples/ServicesActivity.java
@@ -17,7 +17,7 @@ public class ServicesActivity extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_services);
+        setContentView(R.layout.activity_services);
         initViews();
     }
 
